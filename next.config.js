2e46c9a605252b329983190dddd70460ece4915d
jdt10 <jdt10@sfu.ca>
2022-02-26 10:10:50 -0800

    Fonts bug

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  webpack(config) {
-    config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
-    config.experiments = { asyncWebAssembly: true, layers: true };
-    return config;
-  },
-};
