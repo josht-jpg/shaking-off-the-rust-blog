c607d117e61ee896fc5326488f70de154f6c1696
jdt10 <jdt10@sfu.ca>
2022-02-26 09:32:30 -0800

    Pls build, yo

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   reactStrictMode: true,
   webpack(config) {
     config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
-    config.experiments = { asyncWebAssembly: true };
+    config.experiments = { asyncWebAssembly: true, layers: true };
     return config;
   },
 };
